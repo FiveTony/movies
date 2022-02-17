1669c4f1b85fa8c84815bea4d22028852be4ef96
FiveTony <a.malynov@wim.agency>
2022-02-17 17:40:14 +0300

    project complete

---
+++ src/component/MovieList.jsx
@@ -0,0 +1,14 @@
+import { Movie } from "./Movie";
+
+function MovieList(props) {
+  const { movies = [] } = props;
+  return (
+    <div className="movies">
+      {movies.length ? (movies.map((movie) => {
+        return <Movie key={movie.imdbID} {...movie} />;
+      })) : <h4>Nothing...</h4>}
+    </div>
+  );
+}
+
+export { MovieList };
